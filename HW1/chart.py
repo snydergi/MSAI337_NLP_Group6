f68eaa352b49b41487893f30de237119756ab3f1
Joseph Blom <jblom5400@gmail.com>
2025-04-29 14:52:54 -0500

    update graphing function

--- HW1/chart.py
+++ HW1/chart.py
@@ -4,9 +4,26 @@ import matplotlib.pyplot as plt
 with open('perps.pickle', 'rb') as f:
     data = pickle.load(f)
 
-plt.plot(data)
-plt.title('Graph of List Data')
-plt.xlabel('Index')
-plt.ylabel('Value')
-plt.grid(True)
+with open('perpsTest.pickle', 'rb') as f:
+    dataTest = pickle.load(f)
+
+# Determine the maximum length
+max_len = max(len(data), len(dataTest))
+
+# Create a range of x values starting at 1
+x_values = list(range(1, max_len + 1))
+
+plt.plot(x_values[: len(data)], data, label='perps')
+plt.plot(x_values[: len(dataTest)], dataTest, label='perpsTest')
+plt.title('Perplexities')
+plt.xlabel('Epoch')
+plt.ylabel('Perplexity')
+plt.legend()
+
+# Set x-ticks at every whole number starting from 1
+plt.xticks(x_values)
+
+# Remove grid
+# plt.grid(False) is default, so just don’t call plt.grid(True)
+
 plt.show()
